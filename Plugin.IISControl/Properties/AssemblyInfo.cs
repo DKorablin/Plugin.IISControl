cdcac549a465ecd8fc9f1c5048dac386c57c5c23
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 23:31:37 +0300

    Unified plugin deployment

--- Plugin.IISControl/Properties/AssemblyInfo.cs
+++ Plugin.IISControl/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("d8158539-81de-4d83-9087-ece37ea20de3")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.IISControl")]
 #else
 
-[assembly: AssemblyTitle("Plugin.IISControl")]
 [assembly: AssemblyDescription("Internet Information Service client application")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.IISControl")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2011-2024")]
 #endif
